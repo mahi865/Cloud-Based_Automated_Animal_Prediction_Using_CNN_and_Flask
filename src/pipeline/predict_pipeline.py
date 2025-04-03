15b5cbf3c0745e299d8af4c7e4e9ea64f325803b
mahidhar <mahidharavula3@gmail.com>
2025-04-03 14:53:22 -0400

    code fixed

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,30 +2,36 @@ import sys
 import os
 import numpy as np
 import pandas as pd
+from PIL import Image
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model  # Import the correct function for loading the model
 from src.exception import CustomException
 from src.utils import load_object
+
+
+
 
 class PredictPipeline:
     def __init__(self):
         try:
             self.model_path = os.path.join("artifacts", "model.h5")
             self.preprocessor_path = os.path.join("artifacts", "preprocessor.pkl")
-            self.model = load_object(file_path=self.model_path)
+            self.model = load_model(self.model_path)  # Use load_model instead of load_object
             self.preprocessor = load_object(file_path=self.preprocessor_path)
         except Exception as e:
             raise CustomException(e, sys)
 
     def predict(self, image_path: str):
         try:
-            test_image = image.load_img(image_path, target_size=(64, 64))
-            test_image = image.img_to_array(test_image)
+            data = CustomData(image_path)
+            test_image = data.get_data_as_image()
             test_image = np.expand_dims(test_image, axis=0)
-            data_scaled = self.preprocessor.transform(test_image)
+            data_scaled = self.preprocessor.transform(test_image.reshape(1, -1))  # Flatten the image for the scaler
             preds = self.model.predict(data_scaled)
             return "dog" if preds[0][0] == 1 else "cat"
         except Exception as e:
             raise CustomException(e, sys)
+
 
 class CustomData:
     def __init__(self, image_path: str):
@@ -33,6 +39,8 @@ class CustomData:
 
     def get_data_as_image(self):
         try:
-            return image.load_img(self.image_path, target_size=(64, 64))
+            img = image.load_img(self.image_path, target_size=(64, 64))
+            img = image.img_to_array(img)
+            return img
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
+from src.exception import CustomException
 import pickle
+import sys
+import tensorflow as tf
+
 
 def save_object(obj, file_path: str):
-    """
-    Save an object to a file using pickle.
-    """
+    """Save an object using pickle."""
     try:
         with open(file_path, 'wb') as file:
             pickle.dump(obj, file)
@@ -11,11 +13,12 @@ def save_object(obj, file_path: str):
         raise CustomException(f"Error saving object: {e}", sys)
 
 def load_object(file_path: str):
-    """
-    Load an object from a file using pickle.
-    """
+    """Load an object from a file using pickle or TensorFlow (for .h5 models)."""
     try:
-        with open(file_path, 'rb') as file:
-            return pickle.load(file)
+        if file_path.endswith(".h5"):
+            return tf.keras.models.load_model(file_path)  # Load TensorFlow model
+        else:
+            with open(file_path, 'rb') as file:
+                return pickle.load(file)
     except Exception as e:
         raise CustomException(f"Error loading object: {e}", sys)
