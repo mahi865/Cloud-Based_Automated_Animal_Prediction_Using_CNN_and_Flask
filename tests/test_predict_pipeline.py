2c360ca31bc2761747191bb7fb64d54e9b1ce025
mahidhar <mahidharavula3@gmail.com>
2025-04-09 01:21:24 -0400

    Add GitHub Actions workflow for Python tests

--- tests/test_predict_pipeline.py
+++ tests/test_predict_pipeline.py
@@ -4,5 +4,5 @@ from src.pipeline.predict_pipeline import PredictPipeline
 def test_predict_pipeline():
     pipeline = PredictPipeline()
     # Replace 'path_to_test_image.jpg' with the path to a sample test image
-    result = pipeline.predict("dataset\\test_set\\dogs\\dog.4015.jpg")
-    assert result in ["cat", "dog"], "Prediction should be either 'cat' or 'dog'"
+    result = pipeline.predict("dataset\test_set\dogs\dog.4015.jpg")
+    assert result in ["cat", "dog"], "Prediction must be either 'cat' or 'dog'"
