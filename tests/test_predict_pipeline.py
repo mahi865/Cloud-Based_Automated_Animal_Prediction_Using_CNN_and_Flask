a7cc7a7fab89dd4be7259ca476048b07c0324d3e
mahidhar <mahidharavula3@gmail.com>
2025-04-09 11:42:01 -0400

    Add test image

--- tests/test_predict_pipeline.py
+++ tests/test_predict_pipeline.py
@@ -1,9 +1,11 @@
 import pytest
 from src.pipeline.predict_pipeline import PredictPipeline
+import os
 
 def test_predict_pipeline():
+    print("Current Working Directory:", os.getcwd())  # Debugging
     pipeline = PredictPipeline()
-    # Use absolute path for the test image
-    result = pipeline.predict("C:/Users/mahid/Downloads/S37 - dataset/dataset/test_set/dogs/dog.4015.jpg")
+    # Use a relative path
+    result = pipeline.predict("dataset/test_set/dogs/dog.4001.jpg")
     print(f"Prediction Result: {result}")  # Print the result
     assert result in ["cat", "dog"], "Prediction must be either 'cat' or 'dog'"
