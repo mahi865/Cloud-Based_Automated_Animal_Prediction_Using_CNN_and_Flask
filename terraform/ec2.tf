7ba89e93c3e4eee5fe1e28df8b318dac981c7f60
mahidhar <mahidharavula3@gmail.com>
2025-04-23 15:29:42 -0400

    added changes

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,26 +1,4 @@
-# Security Group
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow access to Flask app"
 
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from anywhere
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # Allow all outbound traffic
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "FlaskAppSecurityGroup"
-  }
-}
 
 # EC2 Instance
 resource "aws_instance" "web" {
